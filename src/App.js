87b8d4260c02a429becc0d5a5b90792abe010ddf
jhchoi08 <jhchoi08@cafe24corp.com>
2021-12-21 12:29:30 +0900

    feat: translation helper component rendering test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { NewHomes, MarketingFooter, FormCheckout, Nav, SearchMessage, CardMessageCollection, ButtonNew, ButtonSubmit, InputTest, SelectTest, SideBar, TestNavBar } from './ui-components';
+import { NewHomes, MarketingFooter, FormCheckout, Nav, SearchMessage, CardMessageCollection, ButtonNew, ButtonSubmit, InputTest, SelectTest, SideBar, TestNavBar, TestMyCardCollection } from './ui-components';
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <TestNavBar position="fixed" width="100%" style={{ zIndex:'1111' }} />
       <div style={{ position:"fixed", top:"60px", right:"0", zIndex:'111', textAlign:"center", backgroundColor:"#fff", borderLeft: "1px solid #ddd", height: "100%" }}>
         <SearchMessage style={{ margin:"16px 0 0" }} />
-        <CardMessageCollection />
+        <TestMyCardCollection />
         <ButtonNew />
         <ButtonSubmit style={{ margin:"0 auto 16px", display:"inline-block"}} />
       </div>
